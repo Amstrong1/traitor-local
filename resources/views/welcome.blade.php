145fe1cd117568fc35bc4687c24284b98cf0470f
Amstrong1 <lewisamstrongh@gmail.com>
2023-08-28 11:54:03 +0100

    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tailwind Starter Template - App Landing Page Template: Tailwind Toolbox</title>
+    <title>Traiteur Local Landing Page</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
 
@@ -269,9 +269,9 @@
             <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
                 <h1
                     class="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">
-                    Main Hero Message to sell your app</h1>
+                    Trouver des traiteurs proches de chez vous</h1>
                 <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">
-                    Sub-hero message, not too long and not too short. Make it just right!</p>
+                    Explorer de nouvelles saveurs et cuisines sans quitter votre maison. Commandez en ligne avec Traiteur Local et profitez d'un monde de goûts!</p>
 
                 <p class="text-blue-400 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in">Télécharger l'app ou
                     voir <a href="{{ route('home.index') }}">le site</a></p>
@@ -290,16 +290,15 @@
             <section class="text-gray-600 body-font">
                 <div class="container px-5 py-24 mx-auto">
                     <div class="flex flex-wrap -m-4">
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/2">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                 <img class="lg:h-48 md:h-36 w-full object-cover object-center"
                                     src="{{ asset('img/food.jpg') }}" alt="blog">
                                 <div class="p-6">
-                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY
+                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Customer
                                     </h2>
                                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Pour vous</h1>
-                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings
-                                        jianbing microdosing tousled waistcoat.</p>
+                                    <p class="leading-relaxed mb-3">Redécouvrez toute la gastronomie francaise proche de chez vous et sans vous déplacer.</p>
                                     <div class="flex items-center flex-wrap ">
                                         <a href="{{ route('register') }}" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Creer un compte
                                             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
@@ -309,37 +308,19 @@
                                                 <path d="M12 5l7 7-7 7"></path>
                                             </svg>
                                         </a>
-                                        <span
-                                            class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
-                                                stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                            </svg>1.2K
-                                        </span>
-                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                                </path>
-                                            </svg>6
-                                        </span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/2">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                 <img class="lg:h-48 md:h-36 w-full object-cover object-center"
-                                    src="{{ asset('img/chief.jpeg') }}" alt="blog">
+                                    src="{{ asset('img/chief.jpg') }}" alt="blog">
                                 <div class="p-6">
-                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY
+                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Traiteur
                                     </h2>
                                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Par vous</h1>
-                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings
-                                        jianbing microdosing tousled waistcoat.</p>
+                                    <p class="leading-relaxed mb-3">Faites découvrir toute la richesse des plats francais grâce à votre savoir faire.</p>
                                     <div class="flex items-center flex-wrap">
                                         <a href="{{ route('register.traitor') }}" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Creer un compte
                                             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
@@ -349,65 +330,6 @@
                                                 <path d="M12 5l7 7-7 7"></path>
                                             </svg>
                                         </a>
-                                        <span
-                                            class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                viewBox="0 0 24 24">
-                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                            </svg>1.2K
-                                        </span>
-                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                                </path>
-                                            </svg>6
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-4 md:w-1/3">
-                            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-48 md:h-36 w-full object-cover object-center"
-                                    src="{{ asset('img/rider.jpeg') }}" alt="blog">
-                                <div class="p-6">
-                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY
-                                    </h2>
-                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-                                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings
-                                        jianbing microdosing tousled waistcoat.</p>
-                                    <div class="flex items-center flex-wrap ">
-                                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
-                                                stroke-width="2" fill="none" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path d="M5 12h14"></path>
-                                                <path d="M12 5l7 7-7 7"></path>
-                                            </svg>
-                                        </a>
-                                        <span
-                                            class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                viewBox="0 0 24 24">
-                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                            </svg>1.2K
-                                        </span>
-                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                                </path>
-                                            </svg>6
-                                        </span>
                                     </div>
                                 </div>
                             </div>
